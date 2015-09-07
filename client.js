2c7540f951326a5c710e7b00add12908a66e58fc
Scott Graves <scottgrvs@gmail.com>
2015-09-06 23:07:24 -0400

    A few minor edits based on linter recommendations.

--- client.js
+++ client.js
@@ -21,42 +21,42 @@ memory.on ('chunk', function (chunk) {
  * of 8 bytes.
  */
 var ByteGroup = function (bytes) {
-	
+
 	this.ascii_frag = document.createDocumentFragment ();
 	this.hex_frag = document.createDocumentFragment ();
-	
-	for (var k=0; k < bytes.length; ++k) {				
-		
+
+	for (var k=0; k < bytes.length; ++k) {
+
 		var hex_node = document.createElement ('div');
 		var ascii_node = document.createElement ('div');
-		
+
 		hex_node.className = 'byte';
 		ascii_node.className = 'ascii';
-				
+
 		this.hex_frag.appendChild (hex_node);
-		this.ascii_frag.appendChild (ascii_node);	
-		
+		this.ascii_frag.appendChild (ascii_node);
+
 		if (k < bytes.length) {
-		
+
 			var byte = bytes[k].toString (16);
 			if (byte.length == 1) {
 				byte = '0' + byte;
 			}
-		
-			hex_node.textContent = byte;				
+
+			hex_node.textContent = byte;
 			if (bytes[k] >= 32 && bytes[k] <= 126) {
-				ascii_node.textContent = String.fromCharCode (bytes[k]);		
+				ascii_node.textContent = String.fromCharCode (bytes[k]);
 			}
 			else {
 				ascii_node.textContent = '.';
 			}
 		}
 	}
-	
+
 	var gap_node = document.createElement ('div');
 	gap_node.className = 'gap';
 	this.hex_frag.appendChild (gap_node);
-}
+};
 
 /**
  * Get notified when a new ROM has been selected.
@@ -73,37 +73,37 @@ ipc.on ('close-file', intro);
 
 function intro () {
 	reset ();
-	memory.loadString ('hexview v0.0')
+	memory.loadString ('hexview v0.0');
 }
 
 /**
  * Reset the view and free up resources.
  */
 function reset () {
-	
+
 	groups.length = 0;
-	
+
 	var addr = document.querySelector ('#addr');
 	var hex = document.querySelector ('#hex');
 	var ascii = document.querySelector ('#ascii');
-	
+
 	addr.innerHTML = '';
 	hex.innerHTML = '';
-	ascii.innerHTML = '';	
+	ascii.innerHTML = '';
 }
 
 /**
  * Render the hex output of the ROM.
  */
 function render () {
-	
+
 	var addr = document.querySelector ('#addr');
 	var hex = document.querySelector ('#hex');
 	var ascii = document.querySelector ('#ascii');
-	
+
 	for (var k=0; k < groups.length; k += groups_per_line) {
-		
-		var address_node = document.createElement ('div');		
+
+		var address_node = document.createElement ('div');
 		var hex_node = document.createElement ('div');
 		var ascii_node = document.createElement ('div');
 
@@ -111,12 +111,12 @@ function render () {
 		hex_node.className = 'row';
 		ascii_node.className = 'row';
 		address_node.classList.add ('row', 'addr');
-		address_node.textContent = pad ((k * groups_per_line).toString (16), 8); 
+		address_node.textContent = pad ((k * groups_per_line).toString (16), 8);
 		addr.appendChild (address_node);
-			
-		for (var j=0; k+j < groups.length && j < groups_per_line; ++j) {			
+
+		for (var j=0; k+j < groups.length && j < groups_per_line; ++j) {
 			hex_node.appendChild (groups[k+j].hex_frag);
-			ascii_node.appendChild (groups[k+j].ascii_frag);	
+			ascii_node.appendChild (groups[k+j].ascii_frag);
 		}
 
 		hex.appendChild (hex_node);
--- memory.js
+++ memory.js
@@ -6,18 +6,18 @@ var ee = require ('events').EventEmitter;
 exports.Memory = function () {
 	this.bytes = null;
 	ee.call (this);
-}
+};
 
 util.inherits (exports.Memory, ee);
 
 exports.Memory.prototype.loadFile = function (path) {
-		
+
 	var obj = this;
 	var stat = fs.statSync (path);
-		
-	this.bytes = new Buffer (stat['size']);			
+
+	this.bytes = new Buffer (stat.size);
 	var readable = fs.createReadStream (path);
-	
+
 	readable
 		.on ('data', function (chunk) {
 			chunk.copy (obj.bytes);
@@ -26,11 +26,11 @@ exports.Memory.prototype.loadFile = function (path) {
 		.on ('end', function () {
 			obj.emit ('done');
 			obj = null;
-		});	
-}
+		});
+};
 
-exports.Memory.prototype.loadString = function (str) {	
+exports.Memory.prototype.loadString = function (str) {
 	this.bytes = new Buffer (str, 'binary');
 	this.emit ('chunk', this.bytes);
 	this.emit ('done');
-}
+};
