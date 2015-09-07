ef63c41aedfae7cb12c248ef6431fbb015eb6277
Scott Graves <scottgrvs@gmail.com>
2015-09-07 00:00:34 -0400

    A few OSX specific things
    * handle the way macs deal with app windows
    
    * attach the OS X application menu

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ var dialog = require ('dialog');
 
 var mainWindow = null;
 
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
 require('crash-reporter').start();
 
 /**
  * Open a ROM for a disassembly.
  */
-function openFile (filenames) {  
+function openFile (filenames) {
   mainWindow.send ('load-file', filenames[0]);
 }
 
@@ -20,17 +28,17 @@ function openFile (filenames) {
  * Prompt the user to select a ROM to open.
  */
 function openFileDialog () {
-  
+
   var filters = [
     { name: 'All Files', extensions: ['*'] }
   ];
-  
-  var options = { 
-    title: "Open File...", 
+
+  var options = {
+    title: "Open File...",
     properties: ['openFile'],
-    filters: filters 
-  }; 
-  
+    filters: filters
+  };
+
   dialog.showOpenDialog (mainWindow, options, openFile);
 }
 
@@ -54,25 +62,25 @@ app.on('open-file', function (event, path) {
  * Initialize the application.
  */
 app.on('ready', function() {
-  
+
   mainWindow = new BrowserWindow ({width: 800, height: 600});
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
-  
+
   mainWindow.webContents.on ('did-finish-load', function () {
     mainWindow.send ('close-file');
   });
-  
+
   var mainmenu = [
     {
       label: 'File',
       submenu: [
-        { 
-          label: 'Open...', 
-          click: openFileDialog 
+        {
+          label: 'Open...',
+          click: openFileDialog
         },
         {
           label: "Close",
@@ -85,7 +93,8 @@ app.on('ready', function() {
       ],
     }
   ];
-  
+
   var menu = Menu.buildFromTemplate (mainmenu);
-  mainWindow.setMenu (menu);  
+  Menu.setApplicationMenu(menu);
+  mainWindow.setMenu (menu);
 });
