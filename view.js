44a47f94e5c23f41a749e76351c38cb6677da0a9
twotymz <joshua.p.may@gmail.com>
2015-09-08 21:16:17 -0400

    Rendering improvements.

--- view.js
+++ view.js
@@ -1,48 +1,9 @@
 
 var ChunkReader = require ('./chunkreader.js');
 
-var ByteGroup = function (bytes) {
-	
-	this.ascii_frag = document.createDocumentFragment ();
-	this.hex_frag = document.createDocumentFragment ();
-	
-	for (var k=0; k < bytes.length; ++k) {				
-		
-		var hex_node = document.createElement ('span');
-		var ascii_node = document.createElement ('span');
-		
-		hex_node.className = 'byte';
-		ascii_node.className = 'ascii';
-				
-		this.hex_frag.appendChild (hex_node);
-		this.ascii_frag.appendChild (ascii_node);	
-		
-		if (k < bytes.length) {
-		
-			var byte = bytes[k].toString (16);
-			if (byte.length == 1) {
-				byte = '0' + byte;
-			}
-		
-			hex_node.textContent = byte;
-			
-			// Add a gap every 8 bytes.
-			if (k+1 % 8 == 0) {
-				var gap_node = document.createElement ('span');
-				gap_node.className = 'gap';
-				this.hex_frag.appendChild (gap_node);
-			}
-							
-			if (bytes[k] >= 32 && bytes[k] <= 126) {
-				ascii_node.textContent = String.fromCharCode (bytes[k]);		
-			}
-			else {
-				ascii_node.textContent = '.';
-			}
-		}
-	}	
-}
-
+/**
+ * Create a new view object.
+ */
 exports.View = function () {
 	this.source = null;
 	this.source_position = 0;
@@ -50,60 +11,112 @@ exports.View = function () {
 	this.render ();
 }
 
+/**
+ * Set the source for a view and update the display.
+ */
 exports.View.prototype.setSource = function (source) {
 	this.source = source;
 	this.source_position = 0;
 	this.render ();	
 }
 
+/**
+ * Set the source position.  Doesn't update the view.
+ */
 exports.View.prototype.setSourcePosition = function (source_position) {
 	this.source_position = source_position;
 }
 
+/**
+ * Render a view worth of content starting at the current source position.
+ */
 exports.View.prototype.render = function () {
 	
+	// Figure out how much content to show.
 	var height = window.innerHeight;
-	var total_lines = Math.round (1.5 * height / 14 + 0.5);
+	var total_lines = Math.round (1.25 * height / 14);
 	
-	var addr = document.querySelector ('#addr');
-	var hex = document.querySelector ('#hex');
-	var ascii = document.querySelector ('#ascii');
-
-	addr.innerHTML = '';
-	hex.innerHTML = '';
-	ascii.innerHTML = '';
+	var container = document.querySelector ('#container');
+	container.innerHTML = '';
 	
 	if (this.source === null) {
 		return;
 	}
 	
+	// Get a chunk of data and render it.	
 	var cr = new ChunkReader.ChunkReader (this.source, 
 										  this.bytes_per_line * total_lines);
 	var chunk = cr.fetch (this.source_position);
 	
 	for (var k=0; k < chunk.length; k += this.bytes_per_line) {
 		
-		var group = new ByteGroup (chunk.slice (k, k + this.bytes_per_line));
-		
-		var address_node = document.createElement ('div');		
-		var hex_node = document.createElement ('div');
-		var ascii_node = document.createElement ('div');
-		var gap_node = document.createElement ('span');
-		
-		gap_node.className = 'gap';
-		address_node.className = 'row';
-		hex_node.className = 'row';
-		ascii_node.className = 'row';
-		address_node.classList.add ('row', 'addr');
-		address_node.textContent = pad ((k * this.bytes_per_line).toString (16), 8); 
-		addr.appendChild (address_node);
-		hex_node.appendChild (group.hex_frag);
-		hex_node.appendChild (gap_node);
-		ascii_node.appendChild (group.ascii_frag);
-		
-		hex.appendChild (hex_node);
-		ascii.appendChild (ascii_node);
+		var line = document.createElement ('div');	
+		var address = k * this.bytes_per_line;
+				
+		line.className = 'row';
+		line.appendChild (gap ());
+		line.appendChild (render_line (address, chunk.slice (k, k + this.bytes_per_line)));		
+		container.appendChild (line);
 	}
+}
+
+/**
+ * Generates document fragments for a line worth of bytes.
+ */
+function render_line (address, bytes) {
+	
+	var address_node = document.createElement ('span');
+	var hex_container = document.createElement ('span');
+	var ascii_container = document.createElement ('span');
+	
+	address_node.className = 'addr';
+	address_node.textContent = pad (address.toString (16), 8);
+				
+	for (var k=0; k < bytes.length; ++k) {
+		
+		var hex_node = document.createElement ('span');
+		var ascii_node = document.createElement ('span');
+		
+		hex_node.className = 'byte';
+		ascii_node.className = 'ascii';
+		hex_container.appendChild (hex_node);
+		ascii_container.appendChild (ascii_node);	
+		
+		var byte = bytes[k].toString (16);
+		if (byte.length == 1) {
+			byte = '0' + byte;
+		}
+	
+		hex_node.textContent = byte;
+		
+		// Add a gap every 8 bytes.
+		if ((k+1) % 8 == 0) {
+			hex_container.appendChild (gap ());
+		}
+						
+		if (bytes[k] >= 32 && bytes[k] <= 126) {
+			ascii_node.textContent = String.fromCharCode (bytes[k]);		
+		}
+		else {
+			ascii_node.textContent = '.';
+		}
+	}
+	
+	var frag = document.createDocumentFragment ();
+	frag.appendChild (address_node);
+	frag.appendChild (hex_container);
+	frag.appendChild (ascii_container);
+	
+	return frag;
+}
+
+/**
+ * Returns a "gap" element.
+ */
+function gap () {
+	var gap = document.createElement ('span');
+	gap.className = 'gap';
+	return gap;
 }
 
 /**
